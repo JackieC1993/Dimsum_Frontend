720acbf5616eaf7ab65936272e0ace3ca72b4a22
JackieC1993 <jackiecheung@pursuit.org>
2023-11-21 16:41:31 -0500

    added changes to the home.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {FaGithub} from "react-icons/fa"
 import { FaLinkedin } from "react-icons/fa";
+import Home from './Home';
 function Footer() {
  return (
     <footer className="dimsums-footer">
@@ -9,6 +10,7 @@ function Footer() {
         <li><a href="https://www.linkedin.com/in/jackie-cheung-a0b446b3/" target="_blank"><FaLinkedin color={"green"} size={"60px"}/> </a></li>
         <li><a href="https://github.com/JackieC1993" target="_blank"><FaGithub color={"black"} size={"60px"}/> </a></li>
       </ul>
+      <Home/>
     </footer>
  );
 }
